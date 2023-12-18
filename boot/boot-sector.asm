d09b674a0af6ebbbb6ed91d97c85b0c8f1073052
Jimmy-Jenga <dylanuk2001@hotmail.co.uk>
2023-12-18 05:52:03 +0000

    Update boot sector and screen functions

--- boot/boot-sector.asm
+++ boot/boot-sector.asm
@@ -27,7 +27,7 @@ load_kernel:
 	call print_nl
 
 	mov bx, KERNEL_OFFSET	; Read from disk and store in 0x1000
-	mov dh, 16				; Our future kernel will be larger
+	mov dh, 32				; Our future kernel will be larger
 	mov dl, [BOOT_DRIVE]
 	call disk_load
 	ret
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,17 @@
-#include "../drivers/ports.h"
+#include "../drivers/screen.h"
+#include "util.h"
 
 void main() {
-	/* Screen cursor position: ask VGA control register (0x3d4) for bytes 
-	   14 = high byte of cursor and 15 = low byte of cursor. */
-	port_byte_out(0x3d4, 14); // Requesting byte 14: high byte of cursor pos
-	// Data is returned in VGA data register (0x3d5)
-	int position = port_byte_in(0x3d5);
-	position = position << 8; // High byte: left shift by 8 bits
+	clear_screen();
 
-	port_byte_out(0x3d4, 15); // Requesting byte 15: low byte of cursor pos
-	position += port_byte_in(0x3d5);
+	// Fill up the screen
+	int i = 0;
+	for (i = 0; i < 24; i++) {
+		char str[255];
+		int_to_ascii(i, str);
+		kprint_at(str, 0, i);
+	}
 
-	// VGA 'cells' consist of character and attribute bytes
-	int offset_from_vga = position * 2; // Each character is 2 bytes: char and attribute
-
-	char *vga = 0xb8000; // VGA address starts here
-	vga[offset_from_vga] = 'X'; // Character byte
-	vga[offset_from_vga + 1] = 0x0f; // Attribute byte: white on black
+	kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+	kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }

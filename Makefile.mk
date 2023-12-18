bef25ec0f6b43c7783cf850229d6b82d33838589
Jimmy-Jenga <dylanuk2001@hotmail.co.uk>
2023-12-18 19:37:51 +0000

    Update Makefile.mk and related files

--- Makefile.mk
+++ Makefile.mk
@@ -1,24 +1,25 @@
-C_SOURCES = $(wildcard kernel/*.c drivers/*.c)
-HEADERS = $(wildcard kernel/*.h drivers/*.h)
+C_SOURCES = $(wildcard kernel/*.c drivers/*.c cpu/*.c)
+HEADERS = $(wildcard kernel/*.h drivers/*.h cpu/*.h)
 
-OBJ = ${C_SOURCES:.c=.o}
+OBJ = ${C_SOURCES:.c=.o cpu/interrupt.o}
 
 CC = /usr/bin/gcc
 GDB = /usr/bin/gdb
 
-CFLAGS = -g
+CFLAGS = -g -m32 -fno-PIC
+LDFLAGS = -no-PIE -m elf_i386
 
 os-image.bin: boot/boot-sector.bin kernel.bin
 	cat $^ > os-image.bin
 
 kernel.bin: boot/kernel-entry.o ${OBJ}
-	ld -o $@ -Ttext 0x1000 $^ --oformat binary
+	ld ${LDFLAGS} -o $@ -Ttext 0x1000 $^ --oformat binary
 
 kernel.elf: boot/kernel-entry.o ${OBJ}
-	ld -o $@ -Ttext 0x1000 $^
+	ld ${LDFLAGS} -o $@ -Ttext 0x1000 $^
 
 run: os-image.bin
-	qemu-system-x86_64 -fda os-image.bin
+	qemu-system-i386 os-image.bin
 
 debug: os-image.bin kernel.elf
 	qemu-system-x86_64 -s -fda os-image.bin &
@@ -28,11 +29,13 @@ debug: os-image.bin kernel.elf
 	${CC} ${CFLAGS} -ffreestanding -c $< -o $@
 
 %.o: %.asm
-	nasm $< -f elf64 -o $@
+	nasm $< -f elf -o $@
 
 %.bin: %.asm
 	nasm $< -f bin -o $@
 
 clean:
 	rm -rf *.bin *.dis *.o os-image.bin *.elf
-	rm -rf kernel/*.o boot/*.bin drivers/*.o
+	rm -rf kernel/*.o boot/*.bin drivers/*.o boot/*.o cpu/*.o
+
+# TODO convert to 32 bit
